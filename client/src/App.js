87b09c76dab110fb0bd9c2a0ad26a4fc70cd3be1
PitherGabriel <pitherga2001@gmail.com>
2026-01-16 21:23:04 +0100

    Added a dedicated  page for every category

--- client/src/App.js
+++ client/src/App.js
@@ -1,95 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { FaSearch, FaBars, FaShoppingBasket, FaMapMarkerAlt } from 'react-icons/fa';
-import EmblaCarousel from './EmblaCarousel';
-import logo_tb from './assests/logo_tb.png'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import PersonalCare from './components/category_pages/PersonalCare';
+import Cleaning from './components/category_pages/Cleaning';
+import FarmingProducts from './components/category_pages/FarmingProducts';
+import CookiesAndSweets from './components/category_pages/CookiesAndSweets';
+import Food from './components/category_pages/Food';
+import AlcoholAndDrinks from './components/category_pages/AlcoholAndDrinks';
+import Others from './components/category_pages/Others';
+
 
 function App() {
-  const [categories, setCategories] = useState([]);
-  const [offers, setOffers] = useState([]);
-
-  // Fetch data from our Node backend
-  useEffect(() => {
-    fetch('https://supermarket-cfwf.onrender.com/api/categories')
-      .then(res => res.json())
-      .then(data => setCategories(data));
-
-    fetch('https://supermarket-cfwf.onrender.com/api/offers')
-      .then(res => res.json())
-      .then(data => setOffers(data));
-  }, []);
-
-  const OPTIONS = { dragFree: true, loop: true }
-  const SLIDE_COUNT = 3
-  const SLIDES = offers
-
   return (
-    <div className="app">
-      {/* Main Navigation */}
-      <nav className="navbar">
-        <div className="container nav-container">
-          <div className="logo">
-            <img src={logo_tb} alt="Logo" className="logo-image" />
-          </div>
-
-          <div className="search-bar">
-            <input type="text" placeholder="¿Qué estas buscando?" />
-          </div>
-
-          <div className="nav-icons">
-            {/* Informational only - no login logic */}
-            <div className="icon-item"><FaBars /></div>
-          </div>
-        </div>
-      </nav>
-
-      {/* Categories Section (Circular Icons) */}
-      <section className="categories-section">
-        <div className="container">
-          <div className="category-grid">
-            {categories.map((cat) => (
-              <div key={cat.id} className="category-item">
-                <div className="icon">
-                  <img src={cat.image_url} className="category-icon"
-                    alt={cat.categoria}
-                    loading="lazy" ></img>
-                </div>
-                <span>{cat.categoria}</span>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      {/*Carousel*/}
-      <EmblaCarousel slides={offers} options={OPTIONS} />
-      {/* Weekly Offers Grid */}
-      <section className="offers-section">
-        <div className="container">
-          <h2 className="section-title">Weekly Highlights</h2>
-          <div className="offers-grid">
-            {offers.map((offer) => (
-              <div key={offer.id} className="offer-card">
-                <div className="card-image">
-                  <span className="badge">OFFER</span>
-                  <img src={offer.image} alt={offer.title} />
-                </div>
-                <div className="card-details">
-                  <span className="category-tag">{offer.category}</span>
-                  <h3>{offer.title}</h3>
-                  <p>{offer.description}</p>
-                  <div className="price-tag">{offer.price}</div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-      {/* Footer */}
-      <footer className="footer">
-        <p>&copy; 2026 Centro Comercial TB</p>
-      </footer>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/supermarket" element={<HomePage />} />
+        <Route path="/sobre_nosotros" element={<AboutPage />} />
+        <Route path="/cuidado_personal" element={<PersonalCare />} />
+        <Route path="/limpieza" element={<Cleaning />} />
+        <Route path="/productos_agropecuarios" element={<FarmingProducts />} />
+        <Route path="/galletas_dulces" element={<CookiesAndSweets />} />
+        <Route path="/alimentacion" element={<Food />} />
+        <Route path="/licores_bebidas" element={<AlcoholAndDrinks />} />
+        <Route path="/otros" element={< Others/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
